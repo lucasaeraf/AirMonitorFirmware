b5890defdd32800932bcc90e19500feefc894c2d
lucasaeraf <lucasaeraf@hotmail.com>
2018-11-03 21:11:38 -0300

    Versão final do firmware

--- SourceAtualizado/SourceAtualizado.ino
+++ SourceAtualizado/SourceAtualizado.ino
@@ -2,11 +2,17 @@
 #include <ESP8266WiFi.h>
 #define DHTPIN D2
 #define DSMPIN D1
+#define DSMPIN25 D4
+#define MQ135PIN D3
+
+bool threshold;
 
 unsigned long duration;
+unsigned long duration25;
 unsigned long starttime;
 unsigned long sampletime_ms = 30000;
 unsigned long lowpulseoccupancy = 0;
+unsigned long lowpulseoccupancy25 = 0;
 
 const char* ssid = "Sidnet 7";
 const char* password = "ehoradoshowporra";
@@ -14,7 +20,9 @@ const char* server = "api.thingspeak.com";
 const String apiKey = "6Q4GC2JNZIQD7Q1R";
 
 float ratio = 0;
+float ratio25 = 0;
 float concentration = 0;
+float concentration25 = 0;
 float temp;
 float humidity;
 DHTesp dht;
@@ -72,6 +80,8 @@ void postData(String data){
 void setup() {
   Serial.begin(115200);
   pinMode(DSMPIN, INPUT);
+  pinMode(DSMPIN25, INPUT);
+  pinMode(MQ135PIN, INPUT);
   connectWiFi();
   
   dht.setup(DHTPIN, DHTesp::DHT22);
@@ -82,6 +92,11 @@ void loop() {
   duration = pulseIn(DSMPIN, LOW);
   lowpulseoccupancy += duration;
 
+  duration25 = pulseIn(DSMPIN25, LOW);
+  lowpulseoccupancy25 += duration25; 
+  
+  threshold = digitalRead(MQ135PIN);
+ 
   if ((millis()-starttime) > sampletime_ms)
   {
     
@@ -98,16 +113,31 @@ void loop() {
     Serial.print("Low Pulse Occupancy: ");
     Serial.print(lowpulseoccupancy);
     Serial.print("\n");
+    
     ratio = lowpulseoccupancy/(sampletime_ms)*10.0;  // Integer percentage 0=>100
     concentration = 0.001915*pow(ratio,2) + 0.09522*ratio - 0.04884; // using spec sheet curve
+
+    ratio25 = lowpulseoccupancy25/(sampletime_ms)*10.0;
+    concentration25 = 0.001915*pow(ratio25,2) + 0.09522*ratio25 - 0.04884;
+
+    Serial.println("concentration 2.5 = " + String(concentration25));
+    Serial.println("concentration 1.0 = " + String(concentration));
+    
     Serial.print("concentration = ");
-    Serial.print(concentration);
-    data += "&field3=" + String(concentration);
-    data += "&field4=1";
+    Serial.print(concentration - concentration25);
+    data += "&field3=" + String(concentration - concentration25);
+    data += "&field4=" + String(threshold);
+    data += "&latitude=-19.868104";
+    data += "&longitude=-43.95538";
+    if (temp != 0 && humidity != 0 && concentration != 0)
+      data += "&status=1";
+    else 
+      data += "&status=0";
     Serial.print(" mg/m^3\n");
     Serial.println(data);
     postData(data);
     lowpulseoccupancy = 0;
+    lowpulseoccupancy25 = 0;
     starttime = millis();
   }
 }
